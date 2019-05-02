42bb7674662cf3eb2122ef06b1e2cecf4ae3f72e
19971013lsc <2328565353@qq.com>
2019-05-02 21:42:49 +0800

    add index and bootstrap

---
+++ src/main/java/life/lsc/community/community/controller/IndexController.java
@@ -0,0 +1,19 @@
+package life.lsc.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+}
