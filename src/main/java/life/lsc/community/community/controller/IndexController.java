a6ca78f188c6dcd7f1ca1c1fddafa5af84fb7e61
19971013lsc <2328565353@qq.com>
2019-05-19 22:23:13 +0800

    page index.html

--- src/main/java/life/lsc/community/community/controller/IndexController.java
+++ src/main/java/life/lsc/community/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.lsc.community.community.controller;
 
+import life.lsc.community.community.dto.PaginationDTO;
 import life.lsc.community.community.dto.QuestionDTO;
 import life.lsc.community.community.mapper.UserMapper;
 import life.lsc.community.community.model.User;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,9 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1")Integer page,
+                        @RequestParam(name = "size", defaultValue = "5")Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -37,8 +41,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList = questionService.list();
-            model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+            model.addAttribute("pagination",pagination);
         return "index";
     }
 }
