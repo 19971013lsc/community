8a9c9e7b7fc725b7769d8e970cfc84d80a889a6d
19971013lsc <2328565353@qq.com>
2019-05-22 21:10:07 +0800

    Interceptors

--- src/main/java/life/lsc/community/community/service/QuestionService.java
+++ src/main/java/life/lsc/community/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.lsc.community.community.service;
 
+
 import life.lsc.community.community.dto.PaginationDTO;
 import life.lsc.community.community.dto.QuestionDTO;
 import life.lsc.community.community.mapper.QuestionMapper;
@@ -13,6 +14,9 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by codedrinker on 2019/5/7.
+ */
 @Service
 public class QuestionService {
 
@@ -23,30 +27,79 @@ public class QuestionService {
     private UserMapper userMapper;
 
     public PaginationDTO list(Integer page, Integer size) {
-        PaginationDTO paginationDTO = new PaginationDTO();
-        Integer totalCount = questionMapper.count();
-        paginationDTO.setPagination(totalCount ,page ,size);
 
-        if (page < 1){
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        Integer totalPage;
+
+        Integer totalCount = questionMapper.count();
+
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
             page = 1;
         }
-        if (page > paginationDTO.getTotalPage()){
-            page = paginationDTO.getTotalPage();
+        if (page > totalPage) {
+            page = totalPage;
         }
+
+        paginationDTO.setPagination(totalPage, page);
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+    }
+
+    public PaginationDTO listPaginationDTO(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        Integer totalPage;
+
+        Integer totalCount = questionMapper.countByUserId(userId);
+
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+        paginationDTO.setPagination(totalPage, page);
 
         //size*(page-1)
         Integer offset = size * (page - 1);
-        List<Question> questions = questionMapper.list(offset,size);
+        List<Question> questions = questionMapper.listByUserId(userId, offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
-        for (Question question : questions){
-          User user = userMapper.findById(question.getCreator());
-          QuestionDTO questionDTO = new QuestionDTO();
-          //把question上的对象拷贝到questionDTO
-          BeanUtils.copyProperties(question,questionDTO);
-          questionDTO.setUser(user);
-          questionDTOList.add(questionDTO);
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
         }
+
         paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
